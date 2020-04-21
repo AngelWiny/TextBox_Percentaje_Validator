b07e40a1a892cc2919cae48c8733d601e9c5c92e
Angel Gustavo Garza Aguilar <angel_winy@live.com.mx>
2020-04-20 20:25:13 -0500

    Initial commit

---
+++ TextBox_Percentaje_Validator/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtpercentaje = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtpercentaje
+        '
+        Me.txtpercentaje.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpercentaje.Location = New System.Drawing.Point(34, 39)
+        Me.txtpercentaje.Name = "txtpercentaje"
+        Me.txtpercentaje.Size = New System.Drawing.Size(176, 38)
+        Me.txtpercentaje.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(260, 139)
+        Me.Controls.Add(Me.txtpercentaje)
+        Me.Name = "Form1"
+        Me.Text = "Validacion de porcentaje"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtpercentaje As TextBox
+End Class
---
+++ TextBox_Percentaje_Validator/Form1.vb
@@ -0,0 +1,79 @@
+﻿
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Sub txtpercentaje_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtpercentaje.KeyPress
+
+        'Dim rex As Regex = New Regex("^(\d+\d+|\d+[.]\d+\d)|(100.00)%?$")
+        'Dim match As Match = rex.Match(txtpercentaje.Text)
+        'If match.Success Then
+        '    e.Handled = True
+        'End If
+        e.Handled = Not ValidaCaracter(e.KeyChar)
+    End Sub
+
+    Private Function ValidaCaracter(ByVal Character As Char) As Boolean
+
+        If Character = vbBack Then 'Valida si es tecla borrar
+            Return True
+        End If
+
+        If Character = "."c Then
+            Return PuntoValido()
+        End If
+
+        If Char.IsNumber(Character) Then 'Valida si es numero
+            Return ValorValido(Character)
+        End If
+
+        Return False
+    End Function
+
+    Function ValorValido(ByVal Character As Char) As Boolean
+        If Not TienePunto() Then
+            If EsDecimo() Then
+                If Not EsCentecimo() Then
+                    If Character = "0" Then
+                        Return True
+                    End If
+                End If
+            Else
+                If txtpercentaje.Text.Length < 2 Then
+                    Return True
+                End If
+            End If
+
+        Else
+            If txtpercentaje.Text.Split(".")(1).Length <= 1 Then
+                Return True
+            End If
+        End If
+
+        Return False
+    End Function
+
+    Private Function EsDecimo() As Boolean
+        Return txtpercentaje.Text.StartsWith("10")
+    End Function
+
+    Private Function EsCentecimo() As Boolean
+        Return txtpercentaje.Text.StartsWith("100")
+    End Function
+
+    Function PuntoValido() As Boolean
+        If TienePunto() Then
+            Return False
+        End If
+
+        If txtpercentaje.Text.Length <= 3 And txtpercentaje.Text.Length > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Function TienePunto() As Boolean
+        Return txtpercentaje.Text.Contains(".")
+    End Function
+
+End Class
